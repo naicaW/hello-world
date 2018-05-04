1b25e14c0174e75fc73f5e441ebf253a0d94d6d1
naicaW <31607084+naicaW@users.noreply.github.com>
2018-05-04 17:57:39 +0800

    Update HellowWorld.java

--- HellowWorld.java
+++ HellowWorld.java
@@ -3,6 +3,5 @@ package com.helloworld.demo;
 public class HellowWorld {
     public static void main(String[] args){
         System.out.println("HellowWorld");
-        System.out.println("科贝乐");
     }
 }
